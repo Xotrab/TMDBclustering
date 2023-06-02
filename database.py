532fc115d861c6aab05090930e46d57721b4147f
Xotrab <mokrylisc@gmail.com>
2023-06-02 14:10:05 +0200

    Added the method for creating all the db tables based on the created classes

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine, text
 from config import connection_string
+from models.base import Base
+from models.movie import Movie
+from models.genre import Genre
 
 engine = create_engine(connection_string, echo=True)
 
-with engine.connect() as connection:
-    result = connection.execute(text('select "Hello"'))
+def create_tables():
+    print('CREATING TABLES >>>>')
+    Base.metadata.create_all(bind=engine)
 
-    print(result.all())
+# with engine.connect() as connection:
+#     result = connection.execute(text('select "Hello"'))
+
+#     print(result.all())
 
