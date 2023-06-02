7002b2d3708e1f8b23e44300059a083a119674fa
Xotrab <mokrylisc@gmail.com>
2023-06-02 14:07:41 +0200

    Added the Genre object representing the genres table

---
+++ models/genre.py
@@ -0,0 +1,11 @@
+from models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Genre(Base):
+    __tablename__ = 'genres'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Genre id={self.id}, name={self.name}>'
