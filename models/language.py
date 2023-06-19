ac14a0d7ca5e34aaa0b7b2a081b64495274969d5
Xotrab <mokrylisc@gmail.com>
2023-06-19 17:46:47 +0200

    Language now maps english_name property instead of name property to avoid native names

--- models/language.py
+++ models/language.py
@@ -6,14 +6,14 @@ class Language(Base):
     __tablename__ = 'languages'
 
     iso_639_1: Mapped[str] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    english_name: Mapped[str] = mapped_column(nullable=False)
 
     def __repr__(self) -> str:
-        return f'<Language iso_639_1={self.iso_639_1}, name={self.name}>'
+        return f'<Language iso_639_1={self.iso_639_1}, english_name={self.english_name}>'
 
 class LanguageSchema(Schema):
     iso_639_1 = fields.String()
-    name = fields.String()
+    english_name = fields.String()
 
     #Exclude the unknown JSON fields from the deserialization
     class Meta:
