f71beb9a991e366737af269901688dfd6d977327
Xotrab <mokrylisc@gmail.com>
2023-06-20 17:03:06 +0200

    The movie data is now fetched from the json file containing movie ids

--- fetch_tmdb_data.py
+++ fetch_tmdb_data.py
@@ -1,19 +1,33 @@
 
 from database_facade import db_facade
 from tmdb_api_service import api_service
+from config import movie_ids_json_path, json_encoding
+import json
 
-print('FETCHING TMDB DATA >>>>')
-movie = api_service.get_movie_details(1098160)
+num_lines = 5
 
-reviews = api_service.get_movie_reviews(1098160)
+with open(movie_ids_json_path, "r", encoding=json_encoding) as file:
+    line_count = 0
+    for line in file:
+        line_count += 1
+        json_data = json.loads(line)
+        movie_id = json_data.get("id")
+        if movie_id is not None:
+            print(f'FETCHING TMDB DATA FOR MOVIE ID={movie_id} >>>>')
+            movie = api_service.get_movie_details(movie_id)
 
-movie.reviews = reviews
+            reviews = api_service.get_movie_reviews(movie_id)
 
-print('ADDING DATA TO THE DATABASE >>>>')
-db_facade.add_many([movie])
+            movie.reviews = reviews
 
-if (movie.poster_path):
-    print('FETCHING TMDB MOVIE POSTER >>>>')
-    api_service.save_image(movie.poster_path)
+            print('ADDING DATA TO THE DATABASE >>>>')
+            db_facade.add_many([movie])
 
-print('FINISHED >>>>')
+            if (movie.poster_path):
+                print('FETCHING TMDB MOVIE POSTER >>>>')
+                api_service.save_image(movie.poster_path)
+
+            print('FINISHED >>>>')
+
+        if line_count == num_lines:
+            break
