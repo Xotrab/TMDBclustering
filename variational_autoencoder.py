e96296f42a7dbfc9ff38d6ed4c446feb18048288
Xotrab <mokrylisc@gmail.com>
2023-06-26 19:47:50 +0200

    Added the decoder architecture, and implemented the custom train method

--- variational_autoencoder.py
+++ variational_autoencoder.py
@@ -1,17 +1,21 @@
 import tensorflow as tf
 from keras import layers
+from tqdm import tqdm
+import numpy as np
 
-class VariationalAutoencoder(tf.Keras.model):
+class VariationalAutoencoder(tf.keras.Model):
 
-    def __init__(self, latent_dim, input_width, input_height):
+    def __init__(self, latent_dim, input_width, input_height, optimizer=tf.keras.optimizers.Adam()):
         super(VariationalAutoencoder, self).__init__()
 
         self.latent_dim = latent_dim
         self.input_width = input_width
         self.input_height = input_height
+        self.optimizer = optimizer
 
         # Encoder
-        self.encoder_input = tf.keras.Input(shape=(self.input_width, self.input_height, 3), name='encoder_input')
+        self.encoder_input = tf.keras.Input(shape=(self.input_width, self.input_height, 3), name='input')
+
         self.encoder = self.build_encoder()
 
         # Decoder
@@ -31,10 +35,11 @@ class VariationalAutoencoder(tf.Keras.model):
         # Define convolution layers
         x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(self.encoder_input)
         x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+        x = layers.Conv2D(128, 3, activation="relu", strides=2, padding="same")(x)
 
         # Flatten the output and pass it throught the dense layer
         x = layers.Flatten()(x)
-        x = layers.Dense(16, activation="relu")(x)
+        x = layers.Dense(self.latent_dim, activation="relu")(x)
 
         # Define the mean and variance vectors
         z_mean = layers.Dense(self.latent_dim, name="z_mean")(x)
@@ -47,16 +52,26 @@ class VariationalAutoencoder(tf.Keras.model):
         return tf.keras.Model(self.encoder_input, [z_mean, z_log_var, z], name="encoder")
     
     def build_decoder(self):
-        pass
+        x = layers.Dense(self.input_width // 8 * self.input_height // 8 * 128, activation="relu")(self.decoder_input)
+        x = layers.Reshape((self.input_width // 8, self.input_height // 8, 128))(x)
+        x = layers.Conv2DTranspose(128, 3, activation="relu", strides=2, padding="same")(x)
+        x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+        x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+        decoder_output = layers.Conv2DTranspose(3, 3, activation="sigmoid", padding="same")(x)
+
+        # Instantiate and return the decoder model
+        return tf.keras.Model(self.decoder_input, decoder_output, name="decoder")
 
     # Forward pass of the model
     def call(self, input):
-        _, _, z = self.encoder(input)
-        return self.decoder(z)
+        z_mean, z_log_var, z = self.encoder(input)
+        decoder_output = self.decoder(z)
+
+        return decoder_output, z_mean, z_log_var, z
     
     def vae_loss(self, input, output, z_mean, z_log_var):
         # Calculate the reconstruction loss using the binary crossentropy
-        reconstruction_loss = tf.keras.losses.binary_crossentropy(input, output)
+        reconstruction_loss = tf.keras.losses.binary_crossentropy(input, output) #Maybe should use MSE instead?
 
         # Reduce the loss by taking the mean across all dimensions but the batch dimension
         reconstruction_loss = tf.reduce_mean(reconstruction_loss, axis=(1, 2))
@@ -67,12 +82,32 @@ class VariationalAutoencoder(tf.Keras.model):
         return reconstruction_loss + kl_loss
     
     def compile_model(self):
-        self.compile(optimizer=tf.keras.optimizers.Adam(), loss=self.vae_loss)
+        self.compile(optimizer=self.optimizer)
 
-    def train(self, x_train, x_test, epochs, batch_size):
-        self.fit(x_train, x_train, epochs=epochs, batch_size=batch_size, validation_data=(x_test, x_test))
+    def train(self, data_loader, epochs):
+        # x_train, x_test = data_loader.get_train_test_data()
+        # self.fit(x_train, x_train, batch_size=data_loader.batch_size, epochs=epochs, validation_data=(x_test, x_test))
+        # self.fit(data_loader, epochs=epochs)
+         for epoch in range(epochs):
+
+            progress_bar = tqdm(data_loader, desc=f"Epoch {epoch+1}/{epochs}", unit="batch")
+
+            for batch in progress_bar:
+                
+                with tf.GradientTape() as tape:
+                    decoder_output, z_mean, z_log_var, z = self.call(batch)
+                    loss = self.vae_loss(batch, decoder_output, z_mean, z_log_var)
+                
+                gradients = tape.gradient(loss, self.trainable_variables)
+                self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+                # Update the progress bar description with the current loss
+                progress_bar.set_postfix({"Loss": loss.numpy()})
+
+            # Close the progress bar after each epoch
+            progress_bar.close()
 
     def get_feature_vector(self, input):
-        _, _, z = self.encoder(input)
-        return z
+        z_mean, _, _ = self.encoder(input)
+        return z_mean
         
